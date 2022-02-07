724ef4b7d513d5ddc150f305568d55ee2957cdac
Jaime de Greiff <alegreiff@gmail.com>
2022-02-07 11:10:36 -0500

    pollero

--- src/components/UI/MenuSuperior.jsx
+++ src/components/UI/MenuSuperior.jsx
@@ -141,7 +141,7 @@ const MenuSuperior = ({ acceso, color }) => {
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
-                <span>FOTO</span>
+                <span>p o l l e r o</span>
               </IconButton>
             </Tooltip>
             <Menu
